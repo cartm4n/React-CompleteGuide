25fc7c188dd9fc78df01e564260f271d15666161
JOMP <Johannes.Meyer-Pilz@sms-group>
2025-10-15 11:05:03 +0200

    js refresher import/export group multiple exported things into one object

--- 2 JavaScript Refresher/00-starting-js-proj/scripts/app.js
+++ 2 JavaScript Refresher/00-starting-js-proj/scripts/app.js
@@ -1,7 +1,11 @@
 import { a,b } from "./util.js";
+import * as util from "./util.js";
 import asdf from "./util.js";
 
 console.log(asdf);
 console.log("................");
 console.log(a);
 console.log(b);
+console.log("................");
+console.log(util.a);
+console.log(util.b);
