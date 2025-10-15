8e5fadf1128be94acecc2253af3fec57ae7e16ec
JOMP <Johannes.Meyer-Pilz@sms-group>
2025-10-15 11:45:41 +0200

    js refresher variables & operators

--- 2 JavaScript Refresher/00-starting-js-proj/scripts/app.js
+++ 2 JavaScript Refresher/00-starting-js-proj/scripts/app.js
@@ -9,3 +9,26 @@ console.log(b);
 console.log("................");
 console.log(util.a);
 console.log(util.b);
+
+// Variables & Values
+
+/// Value:
+console.log("Hello World!");
+
+/// Variable:
+let userMessage = "Hello World!";
+console.log(userMessage);
+userMessage = "Hello george!";
+console.log(userMessage);
+
+/// const:
+const pi = 3.14159;
+console.log(pi);
+//pi = 5; // This will cause an error
+
+// revisiting operators
+console.log("hello" + " world");
+console.log("hello" + 5);
+console.log("hello" + true);
+console.log(5 + 5);
+console.log(10 === 5);
