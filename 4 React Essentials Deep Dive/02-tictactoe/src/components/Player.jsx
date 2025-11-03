9bbd131d7af47d6b3cafb0e54288cd71613c8471
Cartm4n <cartm4n90@googlemail.com>
2025-11-03 15:27:35 +0100

    75 own implementation

--- 4 React Essentials Deep Dive/02-tictactoe/src/components/Player.jsx
+++ 4 React Essentials Deep Dive/02-tictactoe/src/components/Player.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react";
 export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
+
   return (
     <li>
       <span className="player">
-        <span className="player-name">{name}</span>
+        {isEditing ? <input className="player-name" defaultValue={name}/> : <span className="player-name">{name}</span>}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button>Edit</button>
+      <button onClick={()=>setIsEditing(!isEditing)}>{isEditing? "Save" :"Edit"}</button>
     </li>
   );
 }
