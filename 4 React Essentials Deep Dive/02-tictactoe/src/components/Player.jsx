7702ac5a5fc788102cc4f2cb30081d3867ca5ef9
Cartm4n <cartm4n90@googlemail.com>
2025-11-03 16:48:28 +0100

    75 trainer implementation

--- 4 React Essentials Deep Dive/02-tictactoe/src/components/Player.jsx
+++ 4 React Essentials Deep Dive/02-tictactoe/src/components/Player.jsx
@@ -2,13 +2,22 @@ import { useState } from "react";
 export default function Player({ name, symbol }) {
   const [isEditing, setIsEditing] = useState(false);
 
+  function handleEditClick() {
+    setIsEditing(!isEditing);
+  }
+
+  let playerName = <span className="player-name">{name}</span>
+  if (isEditing) {
+    playerName = <input type="text" required />
+  }
+
   return (
     <li>
       <span className="player">
-        {isEditing ? <input className="player-name" defaultValue={name}/> : <span className="player-name">{name}</span>}
+        {playerName}
         <span className="player-symbol">{symbol}</span>
       </span>
-      <button onClick={()=>setIsEditing(!isEditing)}>{isEditing? "Save" :"Edit"}</button>
+      <button onClick={handleEditClick}>{isEditing ? "Save" : "Edit"}</button>
     </li>
   );
 }
