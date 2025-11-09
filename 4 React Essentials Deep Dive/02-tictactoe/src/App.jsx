a374823dcfd7d28cd8d47ff9bd0990d3210fe7d5
Cartm4n <cartm4n90@googlemail.com>
2025-11-09 13:28:01 +0100

    82 Lifting State up

--- 4 React Essentials Deep Dive/02-tictactoe/src/App.jsx
+++ 4 React Essentials Deep Dive/02-tictactoe/src/App.jsx
@@ -1,15 +1,24 @@
+import { useState } from "react";
+
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+
+  function handleSelectSquare(rowIndex, colIndex) {
+    console.log("Selected square at:", rowIndex, colIndex);
+    setActivePlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
+        <ol id="players" className="highlight-players">
           <Player initialName="Player 1" symbol="X" />
           <Player initialName="Player 2" symbol="O" />
         </ol>
-        <GameBoard />
+        <GameBoard onSelectSquare={handleSelectSquare} />
       </div>
     </main>
   );
