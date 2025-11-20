4643911b22f1253bc56dad722674246dc57c952e
Cartm4n <c4rtm4n90@googlemail.com>
2025-11-20 08:49:43 +0100

    90 extract gameboard functionallity to app.jsx to make win calc possible

--- 4 React Essentials Deep Dive/02-tictactoe/src/App.jsx
+++ 4 React Essentials Deep Dive/02-tictactoe/src/App.jsx
@@ -3,6 +3,13 @@ import { useState } from "react";
 import Player from "./components/Player";
 import GameBoard from "./components/GameBoard";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combinations.js";
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -19,6 +26,18 @@ function App() {
   // const [activePlayer, setActivePlayer] = useState("X");
 
   const activePlayer = deriveActivePlayer(gameTurns);
+  let gameBoard = initialGameBoard;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+
+    gameBoard[row][col] = player;
+  }
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const [a, b, c] = combination;
+  }
 
   function handleSelectSquare(rowIndex, colIndex) {
     // setActivePlayer((prevPlayer) => (prevPlayer === "X" ? "O" : "X"));
@@ -48,7 +67,7 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard onSelectSquare={handleSelectSquare} turns={gameTurns} />
+        <GameBoard onSelectSquare={handleSelectSquare} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
--- 4 React Essentials Deep Dive/02-tictactoe/src/components/GameBoard.jsx
+++ 4 React Essentials Deep Dive/02-tictactoe/src/components/GameBoard.jsx
@@ -1,48 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  // Derive game board state from turns prop
-  if (turns.length > 0) {
-    gameBoard = initialGameBoard.map((row) => [...row]); // Deep copy
-    let count = 0;
-    turns.forEach((turn) => {
-      count++;
-      const { row, col } = turn.square;
-      gameBoard[row][col] = turn.player;
-    });
-    console.log(`GameBoard derived state updated with ${count} turns.`);
-  }
-
-  // const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  // function handleSelectSquare(rowIndex, colIndex) {
-  //   console.log(`handleSelectSquare(): Row: ${rowIndex}, Col: ${colIndex}`);
-  //   setGameBoard((prevGameBoard) => {
-  //     const updatedGameBoard = [...prevGameBoard.map((innerArray => [...innerArray]))];
-  //     updatedGameBoard[rowIndex][colIndex] = activePlayerSymbol;
-  //     console.log(updatedGameBoard);
-  //     return updatedGameBoard;
-  //   });
-
-  //   onSelectSquare();
-  // }
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
