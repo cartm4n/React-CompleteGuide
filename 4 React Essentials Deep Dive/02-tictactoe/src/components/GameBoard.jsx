5ec021fe2a027588d94e6f1e7c42cbd81396a5b8
Cartm4n <cartm4n90@googlemail.com>
2025-11-20 08:25:53 +0100

    88 disable buttons conditionally

--- 4 React Essentials Deep Dive/02-tictactoe/src/components/GameBoard.jsx
+++ 4 React Essentials Deep Dive/02-tictactoe/src/components/GameBoard.jsx
@@ -4,12 +4,12 @@ const initialGameBoard = [
   [null, null, null],
 ];
 
-export default function GameBoard({onSelectSquare, turns}) {
+export default function GameBoard({ onSelectSquare, turns }) {
   let gameBoard = initialGameBoard;
 
-  for(const turn of turns) {
-    const {square, player} = turn;
-    const {row, col} = square;
+  for (const turn of turns) {
+    const { square, player } = turn;
+    const { row, col } = square;
 
     gameBoard[row][col] = player;
   }
@@ -47,7 +47,12 @@ export default function GameBoard({onSelectSquare, turns}) {
           <ol>
             {row.map((playerSymbol, colIndex) => (
               <li key={colIndex}>
-                <button onClick={() => onSelectSquare(rowIndex,colIndex)}>{playerSymbol}</button>
+                <button
+                  onClick={() => onSelectSquare(rowIndex, colIndex)}
+                  disabled={playerSymbol !== null}
+                >
+                  {playerSymbol}
+                </button>
               </li>
             ))}
           </ol>
